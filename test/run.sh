675208f6c7657b1651e1ccb39351d71ee3396001
Eigo Shimizu <eigos@hgc.jp>
2015-04-10 11:02:50 +0900

    w3varann support

--- test/run.sh
+++ test/run.sh
@@ -3,14 +3,13 @@
 # Set SGE
 #
 #$ -S /bin/bash # set shell in UGE
-#$ -o /home/eigos/Log
-#$ -e /home/eigos/Log
 #$ -cwd         # execute at the submitted dir
 pwd             # print current working directory
 hostname        # print hostname
 date            # print date
 set -xv
 /usr/local/package/python2.7/2.7.2/bin/python $2 /home/w3varann/tools/Genomon/genomon.py \
-           --config_file /home/w3varann/tools/Genomon/genomon.cfg \
+           --config_file /home/w3varann/tools/Genomon/test/genomon.cfg \
            --job_file $1 \
-           --jobs 8
+           --jobs 8 --verbose 20
+date
