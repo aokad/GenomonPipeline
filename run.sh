7f09bbfe55626956c33d6a2256068a96c373808a
Eigo Shimizu <eigos@hgc.jp>
2015-08-06 10:53:53 +0900

    Test brancking.

--- run.sh
+++ run.sh
@@ -14,6 +14,7 @@ export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PYTHONHOME}/lib:/home/w3varann/.loca
 export R_LIBS=/home/w3varann/.R
 export R_HOME=/home/w3varann/.R
 
+
 /usr/local/package/python2.7/current/bin/python $3 /home/w3varann/tools/Genomon/genomon.py \
            --config_file /home/w3varann/tools/Genomon/samples/genomon.cfg \
            --job_file $1 \
