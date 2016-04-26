128ea8ab6e68cafea6e8ec018982631aa975b494
Kenichi Chiba <kchiba@hgc.jp>
2016-04-26 11:52:47 +0900

    modify setup.py

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ setup(name='genomon_pipeline',
       version='2.2.0',
       description='Python tools for running genomon pipeline for cancer genome and transcriptome sequencing analysis',
       author='Kenichi Chiba, Ai Okada and Yuichi Shiraishi',
-      author_email='kchiba@hgc.jp',
+      author_email='genomon.devel@gmail.com',
       url='https://github.com/Genomon-Project/Genomon.git',
       package_dir = {'': 'scripts'},
       packages=['genomon_pipeline', 'genomon_pipeline.rna_resource', 'genomon_pipeline.dna_resource', 'genomon_pipeline.config'],
       scripts=['genomon_pipeline'],
-      license='GPL-3'
+      license='License of GenomonPipeline'
      )
